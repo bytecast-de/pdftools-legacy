0d4770c2a437135ac9bed678166a65370e775c3b
Benjamin Bieber <benjamin.bieber@vema-eg.de>
2014-11-05 11:47:30 +0100

    neu: repeatBasePages in Erzeugungslogik

--- src/de/vemaeg/pdf/PdfCreator.java
+++ src/de/vemaeg/pdf/PdfCreator.java
@@ -286,7 +286,7 @@ public class PdfCreator {
 		outputStream.close();
 	}	
 	
-	public static void pdfOverlayMulti(InputStream base, Map<InputStream, String> overlays, OutputStream outputStream) throws IOException, DocumentException, PdfException {
+	public static void pdfOverlayMulti(InputStream base, Map<InputStream, String> overlays, OutputStream outputStream, boolean repeatBasePages) throws IOException, DocumentException, PdfException {
 		
 		// optionale Basis-Seite
 		PdfReader baseReader = null;
@@ -321,11 +321,18 @@ public class PdfCreator {
 			}	
 			
 			boolean pageExists = false;
-			if (baseReader != null && baseReader.getNumberOfPages() >= i) {
-				ensurePageExists(document, i, baseReader, i);
+			int basePageCnt = baseReader.getNumberOfPages();
+			if (baseReader != null && (basePageCnt >= i || repeatBasePages)) {
+				int readerPageNum = i;
+				
+				if (repeatBasePages) {
+					readerPageNum = ((i-1) % basePageCnt) + 1;
+				}
+				
+				ensurePageExists(document, i, baseReader, readerPageNum);
 				pageExists = true;
 				PdfContentByte cbDirect = writer.getDirectContent(); 
-				PdfImportedPage basePage = writer.getImportedPage(baseReader, i);	
+				PdfImportedPage basePage = writer.getImportedPage(baseReader, readerPageNum);	
 				cbDirect.addTemplate(basePage, 0, 0);
 			}
 			
